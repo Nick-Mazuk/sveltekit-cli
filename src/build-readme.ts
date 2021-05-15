abe41670247821a0018a8b348816c0411b1eea97
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-15 08:56:32 -0700

    fix: fix readme generation

--- src/build-readme.ts
+++ src/build-readme.ts
@@ -6,16 +6,11 @@ import fs from 'fs-extra'
 const execute = promisify(exec)
 
 const main = async () => {
-    const standardOut = (await execute('node index.js -h')).stdout
-    const helpOutput = (await execute('node index.js -h')).stderr.replace(/\n$/u, '')
+    const output = await execute('node index.js -h')
+    const helpText = output.stdout === '' ? output.stderr : output.stdout
+    const helpOutput = helpText.replace(/\n$/u, '')
     let readmeContents = fs.readFileSync('templates/build-readme/README.md').toString()
     readmeContents = readmeContents.replace('YARGS_HELP_OUTPUT', helpOutput)
-    console.log('stdout')
-    console.log(standardOut)
-    console.log('stderr')
-    console.log(helpOutput)
-    console.log('readme')
-    console.log(readmeContents)
     fs.writeFileSync('README.md', readmeContents)
 }
 
