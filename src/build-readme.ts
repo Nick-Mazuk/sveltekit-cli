c62c15151b010fd547d5176dd82984debcab9ac1
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-05-15 08:54:13 -0700

    ci: test the new readme generator

--- src/build-readme.ts
+++ src/build-readme.ts
@@ -6,9 +6,16 @@ import fs from 'fs-extra'
 const execute = promisify(exec)
 
 const main = async () => {
+    const standardOut = (await execute('node index.js -h')).stdout
     const helpOutput = (await execute('node index.js -h')).stderr.replace(/\n$/u, '')
     let readmeContents = fs.readFileSync('templates/build-readme/README.md').toString()
     readmeContents = readmeContents.replace('YARGS_HELP_OUTPUT', helpOutput)
+    console.log('stdout')
+    console.log(standardOut)
+    console.log('stderr')
+    console.log(helpOutput)
+    console.log('readme')
+    console.log(readmeContents)
     fs.writeFileSync('README.md', readmeContents)
 }
 
